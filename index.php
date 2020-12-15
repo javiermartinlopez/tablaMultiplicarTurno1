785d6dd2be99302b49139fcba13a8755b0c3c84d
javiermartinlopez <javier.martin.lopez.alu@iesjulioverne.es>
2020-12-15 09:19:20 +0100

    Tabla del 3

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-	echo("<br />Tabla del 2");
+	echo("<br />Tabla del 2<br />");
 	for($i=1;$i<=10;$i++){
 		$resultado = 2*$i;
 		echo("2 por $i = $resultado<br />");
 	}
-	echo("<br />Tabla del 3");
+	echo("<br />Tabla del 3<br />");
 	for($i=1;$i<=10;$i++){
 		$resultado = 3*$i;
 		echo("3 por $i = $resultado<br />");
