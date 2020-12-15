f6dd36e8c76724cc7bd92bc3033998dcef06958a
javiermartinlopez <javier.martin.lopez.alu@iesjulioverne.es>
2020-12-15 09:16:32 +0100

    Añado la tabla del 3

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
+	echo("<br />Tabla del 2");
 	for($i=1;$i<=10;$i++){
 		$resultado = 2*$i;
 		echo("2 por $i = $resultado<br />");
+	}
+	echo("<br />Tabla del 3");
+	for($i=1;$i<=10;$i++){
+		$resultado = 3*$i;
+		echo("3 por $i = $resultado<br />");
 	}
 ?>
